ffd1e830daa243dcd756bb5df4d2f30facbd534b
ItaloCobains <monokatchaleger@gmail.com>
2022-10-23 22:47:52 -0300

    att

--- Conta.cs
+++ Conta.cs
@@ -11,14 +11,57 @@ namespace banco
         private Pessoa Titular;
         private string Nconta;
         private string Nagencia;
-        private double Saldo;
+        private float Saldo;
 
-        public Conta(Pessoa titular, string nconta, string nagencia, double saldo)
+        public Conta(Pessoa titular, string nconta, string nagencia, float saldo)
         {
             Titular = titular;
             Nconta = nconta;
             Nagencia = nagencia;
             Saldo = saldo;
+        }
+
+        public bool Sacar(float value)
+        {
+            if (this.Saldo < value)
+            {
+                return false;
+            }
+            if (value < 0 )
+            {
+                return false;
+            }
+            this.Saldo -= value;
+            return true;
+        }
+
+        public bool Depositar(float value)
+        {
+            if (value < 0)
+            {
+                return false;
+            }
+            this.Saldo += value;
+            return true;
+        }
+
+        public bool Transferir(string agencia, string conta, float value)
+        {
+            if (this.Saldo < value)
+            {
+                return false;
+            }
+            if (value < 0)
+            {
+                return false;
+            }
+            this.Saldo -= value;
+            return true;
+        }
+
+        public float saldo()
+        {
+            return Saldo;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,38 +1,77 @@
 ﻿using banco;
 using System;
+using System.Globalization;
 
 class Banco
 {
+    Conta ContaMain;
+    Pessoa PessoaContaMain;
+
+    public void GerarContaMain()
+    {
+        PessoaContaMain = new Pessoa("User", "1", 18, "123.123.123-98", 234234);
+        ContaMain = new Conta(PessoaContaMain, "09923-2", "2345", 400);
+    }
+
     public void MenuInicial()
     {
+        
         string opcao = "";
+        float q;
+
+
 
         while (opcao != "0")
         {
             Console.WriteLine("==================== BEM VINDO AO BANCO ===================");
             Console.WriteLine("0 - Sair.");
-            Console.WriteLine("1 - Criar conta.");
+            Console.WriteLine("1 - Sacar.");
             Console.WriteLine("2 - Depositar.");
-            Console.WriteLine("3 - Sacar.");
-            Console.WriteLine();
+            Console.WriteLine("3 - Transferir.");
+            Console.WriteLine("4 - Ver Saldo.");
             Console.WriteLine("Informe a opção desejada: ");
             opcao = Console.ReadLine();
 
-            switch(opcao)
+            string quantia;
+            switch (opcao)
             {
                 case "0":
                     return;
                 case "1":
-                    Conta
+                    Console.WriteLine("Quando quer sacar ?");
+                    quantia = Console.ReadLine();
+                    q = float.Parse(quantia);
+                    ContaMain.Sacar(q);
+                    break;
+                case "2":
+                    Console.WriteLine("Quanto quer depositar ?");
+                    quantia = Console.ReadLine();
+                    q = float.Parse(quantia);
+                    ContaMain.Depositar(q);
+                    break;
+                case "3":
+                    Console.WriteLine("Digite a agencia: ");
+                    string agencia = Console.ReadLine();
+                    Console.WriteLine("Digite a conta: ");
+                    string conta = Console.ReadLine();
+                    Console.WriteLine("Quanto quer transferir ?");
+                    quantia = Console.ReadLine();
+                    q = float.Parse(quantia);
+                    ContaMain.Transferir(agencia, conta, q);
+                    break;
+                case "4":
+                    float result = ContaMain.saldo();
+                    Console.WriteLine($"{result}");
+                    break;
             }
         }
     }
     
 
-    public static void Main(string[] args)
+    public static void Main()
     {
-        Banco banco = new Banco();  
-
+        Banco banco = new();
+        banco.GerarContaMain();
         banco.MenuInicial();
     }
 }
